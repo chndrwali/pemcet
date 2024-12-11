8613b27b4cfa28314f31af482813c0dc4cbe3674
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-11 15:55:46 +0700

    add: relvant page

---
+++ app/(home)/relevant/card-relevant.tsx
@@ -0,0 +1,28 @@
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+
+const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+
+export const CardRelevant = () => {
+  return (
+    <>
+      <div className="w-full rounded-xl bg-blue-900 p-4 ">
+        <div className=" bg-white rounded-xl">
+          <div className={`${inter.className} px-4 py-1 text-sm`}>
+            <h1 className="">Assalamualaikum warohmatullahi wabarokatuh</h1>
+
+            <p className=""> Puji dan syukur kehadirat Allah SWT. Karena berkat Rahmat dan karunia-nya saya dapat menyelesaikan media pembelejaran berbasis aplikasi android yang di beri nama &quot;WEPCET&quot; </p>
+            <p>
+              Aplikasi WEPCET ini merupakan aplikasi yang didalam nya membahas mengenai materi pembaca cepat, bukan hanya materi saja didalam aplikasi ini juga terdapat beberapa fitur diantara nya yaitu relevansi, materi, simulasi, Quis,
+              serta tes membaca cepat. Aplikasi ini dikemas dengan semenarik mungkin agar peserta didik mudah untuk memahami nya. Saya berharap semoga aplikasi ini bermanfaat untuk menambah ilmu pengetahuan dan dapat menambah wawasan bagi
+              para pengguna
+            </p>
+          </div>
+        </div>
+      </div>
+      <button className="absolute -bottom-14 right-4 flex items-center justify-center w-12 h-12 rounded-full bg-yellow-400 shadow-lg">
+        <Image src="/logo/arrow-right.png" alt="Arrow right" width={24} height={24} />
+      </button>
+    </>
+  );
+};
--- app/(home)/relevant/page.tsx
+++ app/(home)/relevant/page.tsx
@@ -1,11 +1,25 @@
+import { Caveat } from 'next/font/google';
 import { Metadata } from 'next';
+import { CardRelevant } from './card-relevant';
+
+const caveat = Caveat({ subsets: ['latin'], weight: ['400', '700'] });
 
 export const metadata: Metadata = {
   title: 'Relevansi',
 };
 
 const RelevantPage = () => {
-  return <section>Relevant</section>;
+  return (
+    <section className={`${caveat.className} relative flex flex-col z-20`}>
+      <div className="flex items-center justify-center">
+        <h1 className="text-5xl  sm:text-9xl text-white">WEPCET</h1>
+      </div>
+
+      <h2 className="border border-gray-800 rounded-md bg-yellow-400 w-[100px] ml-4 p-1 font-semibold text-lg uppercase text-center">Deskripsi</h2>
+      <h3 className="bg-blue-200 rounded-md border border-gray-800 ml-12  p-1 w-[100px] font font-semibold text-lg text-center uppercase">Produk</h3>
+      <CardRelevant />
+    </section>
+  );
 };
 
 export default RelevantPage;
