5d2bed7cac1161a7a13d621e94ac6ed7c955cd40
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-13 14:41:17 +0700

    add: conclusion

---
+++ app/(home)/learning-materials/understanding/conclusion/card-conclusion.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { ButtonNextPrevious } from '@/components/button-next-previous';
+import { Inter } from 'next/font/google';
+import { useRouter } from 'next/navigation';
+
+const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+
+export const CardConclusion = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="w-full rounded-xl bg-blue-900 p-4 mt-[30px] ">
+        <div className=" bg-white rounded-xl">
+          <div className={`${inter.className} px-4 py-1 space-y-4 text-sm`}>
+            <h2 className="font-semibold text-center">Kesimpulan</h2>
+            <p className="leading-relaxed text-justify">
+              Membaca cepat adalah kemampuan membaca dengan kecepatan tinggi tanpa mengorbankan pemahaman. Membaca cepat bertujuan untuk menghemat waktu sambil tetap memahami inti dari teks yang dibaca.
+            </p>
+            <h2 className="text-center font-semibold">Kata Kunci:</h2>
+            <ul>
+              <ol className="list-decimal text-justify ml-2">
+                <li>
+                  <strong>Skimming</strong> adalah teknik membaca cepat untuk menangkap gagasan utama atau inti dari suatu teks tanpa memperhatikan detailnya.
+                </li>
+                <li>
+                  <strong>Scanning</strong> adalah teknik membaca cepat untuk mencari informasi spesifik dalam sebuah teks.
+                </li>
+                <li>
+                  <strong>Subvokalisasi</strong> adalah kebiasaan mengucapkan kata-kata dalam pikiran atau &quot;membaca dalam hati&quot; saat membaca. Dalam subvokalisasi, pembaca menggerakkan atau membayangkan suaranya sendiri mengucapkan
+                  setiap kata, meskipun suara tersebut tidak terdengar.
+                </li>
+              </ol>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <ButtonNextPrevious onClick={() => router.back()} isLeft={true} />
+      <ButtonNextPrevious onClick={() => router.push('/benefit')} isLeft={false} />
+    </>
+  );
+};
--- app/(home)/learning-materials/understanding/conclusion/page.tsx
+++ app/(home)/learning-materials/understanding/conclusion/page.tsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+import { CardConclusion } from './card-conclusion';
+import { Caveat_Brush } from 'next/font/google';
+
+const caveat = Caveat_Brush({ subsets: ['latin'], weight: ['400'] });
+
 const ConclusionPage = () => {
-  return <></>;
+  return (
+    <section className="relative flex flex-col items-center justify-center z-20">
+      <h1 className={`${caveat.className} text-5xl sm:text-9xl text-white`}>WEPCET</h1>
+      <div className="absolute top-0 left-4 -mt-[30px]">
+        <Image src="/logo/murid.png" alt="murid" width={100} height={100} />
+      </div>
+      <CardConclusion />
+    </section>
+  );
 };
 
 export default ConclusionPage;
