b00b8aa61dcbef42e5c0d6c48f66060af6f94644
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-13 18:17:04 +0700

    add: remove footer

--- app/(home)/learning-materials/understanding/conclusion/page.tsx
+++ app/(home)/learning-materials/understanding/conclusion/page.tsx
@@ -8,7 +8,7 @@ const ConclusionPage = () => {
   return (
     <section className="relative flex flex-col items-center justify-center z-20">
       <h1 className={`${caveat.className} text-5xl sm:text-9xl text-white`}>WEPCET</h1>
-      <div className="absolute top-0 left-4 -mt-[30px]">
+      <div className="absolute top-0 left-4 ">
         <Image src="/logo/murid.png" alt="murid" width={100} height={100} />
       </div>
       <CardConclusion />
